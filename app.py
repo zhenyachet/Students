ea1567781a8b472ded87e710c67c63b09d778f2c
Evgeny Chetvergov <60198003+zhenyachet@users.noreply.github.com>
2022-01-18 18:54:38 +0100

    feat: use case one

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ def hello_world():
     list_of_names_students = []
     for result in results2:
         list_of_names_students.append(result["name"])
-    print(list_of_names_students)
     query3 = """
         match (f:Flat) return ID(f) as flat
     """
@@ -44,7 +43,6 @@ def hello_world():
     for result in results3:
         list_of_ids_flats.append(result["flat"])
 
-    print(list_of_ids_flats)
 
     for i in range(len(list_of_ids_flats)):
         query4 = f"MATCH(s:Student),(f:Flat) WHERE s.name = '{list_of_names_students[i]}' AND ID(f) = {list_of_ids_flats[i]} CREATE (s)-[r:lives_in]->(f) RETURN type(r)"
@@ -53,7 +51,6 @@ def hello_world():
     # # delete the settled students
     n = len(list_of_ids_flats)
     unsettled_students = list_of_names_students[n:]
-    print(unsettled_students)
     query = """match(s: Student)-[: lives_in]->(f:Flat)
     with f as flat, s as student, count(s) as rels, avg(s.age) as avg_age, collect(
             s) as students where rels < flat.numberOfRooms return flat, students Order by student.sex asc
@@ -61,19 +58,31 @@ def hello_world():
     session.run(query)
 
     # find the appartment for the rest of the students
+    for student in unsettled_students:
+        query = """ match (f:Flat)<-[r2]-(s:Student) 
+                    match (s4:Student{name:"%s"}) 
+                    with f as flat, avg(s.age) as avg_age , count(r2) as rels, s4.age as age_of_person, (apoc.coll.occurrences(collect(s.sex), s4.sex) * 4) as result_of_sex,(apoc.coll.occurrences(collect(s.nationality), s4.nationality) * 4) as result_of_nationality, abs(s4.age - avg(s.age))*(-0.25) as result_of_age 
+                    where rels<flat.numberOfRooms return flat,rels, avg_age, result_of_age,result_of_sex, result_of_nationality, (result_of_sex+ result_of_age+result_of_nationality)as weight
+                    order by weight desc
+                    limit 1
+        """ % student
+        results = session.run(query)
+        flat_name = ''
+        for result in results:
+            dictionary = (result.data())
+            flat_name = dictionary['flat']['name']
 
-    query = """ match (f:Flat)<-[r2]-(s:Student) 
-                match (s4:Student{name:"Catrin Brooks"}) 
-                with f as flat, avg(s.age) as avg_age , count(r2) as rels, s4.age as age_of_person, (apoc.coll.occurrences(collect(s.sex), s4.sex) * 4) as result_of_sex,(apoc.coll.occurrences(collect(s.nationality), s4.nationality) * 4) as result_of_nationality, abs(s4.age - avg(s.age))*(-0.25) as result_of_age 
-                where rels<flat.numberOfRooms return flat,rels, avg_age, result_of_age,result_of_sex, result_of_nationality, (result_of_sex+ result_of_age+result_of_nationality)as weight
-                order by weight desc
-                limit 1
-    """
-    results = session.run(query)
-    for result in results:
-        dictionary = (result.data())
-        flat_name = dictionary['flat']['name']
-    print(data)
+
+        query = f"""
+        MATCH
+        (a:Student),
+        (b:Flat)
+        WHERE a.name = '{student}' AND b.name = '{flat_name}'
+        CREATE (a)-[r:RELTYPE]->(b)
+        RETURN type(r)
+        """
+        session.run(query)
+
     return 'Hello World!'
 
 
